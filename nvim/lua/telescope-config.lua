8caaaae2428e1b866a247f6f364d4cd435728d5e
Jorge Befan <jorge.befan@gmail.com>
2022-02-14 23:05:41 +0100

    add functionality for Telescope, function to fall back to find_files if not a git project

--- nvim/lua/telescope-config.lua
+++ nvim/lua/telescope-config.lua
@@ -1,67 +1,68 @@
 local previewers = require("telescope.previewers")
 local Job = require("plenary.job")
 local new_maker = function(filepath, bufnr, opts)
-  filepath = vim.fn.expand(filepath)
-  Job:new({
-    command = "file",
-    args = { "--mime-type", "-b", filepath },
-    on_exit = function(j)
-      local mime_type = vim.split(j:result()[1], "/")[1]
-      if mime_type == "text" then
-        previewers.buffer_previewer_maker(filepath, bufnr, opts)
-      else
-        -- maybe we want to write something to the buffer here
-        vim.schedule(function()
-          vim.api.nvim_buf_set_lines(bufnr, 0, -1, false, { "BINARY" })
-        end)
-      end
-    end
-  }):sync()
+	filepath = vim.fn.expand(filepath)
+	Job
+		:new({
+			command = "file",
+			args = { "--mime-type", "-b", filepath },
+			on_exit = function(j)
+				local mime_type = vim.split(j:result()[1], "/")[1]
+				if mime_type == "text" then
+					previewers.buffer_previewer_maker(filepath, bufnr, opts)
+				else
+					-- maybe we want to write something to the buffer here
+					vim.schedule(function()
+						vim.api.nvim_buf_set_lines(bufnr, 0, -1, false, { "BINARY" })
+					end)
+				end
+			end,
+		})
+		:sync()
 end
 
 -- You dont need to set any of these options. These are the default ones. Only
 -- the loading is important
-require('telescope').setup {
-    pickers = {
-        find_files = {
-            hidden = true,
-            no_ignore = true,
-        }
-    },
-    defaults = {
-    prompt_prefix = '   ',
-        -- buffer_previewer_maker = new_maker,
-        file_ignore_patterns = {
-            "node_modules/",
-            '.git/',
-            -- 'DS_'
-        },
-        vimgrep_arguments = {
-      'rg',
-      '--color=never',
-      '--no-heading',
-      '--with-filename',
-      '--line-number',
-      '--column',
-      '--smart-case',
-      '--ignore-file',
-      -- '.gitignore'
-    },
-    },
-    extensions = {
-        fzf = {
-            fuzzy = true,                    -- false will only do exact matching
-            override_generic_sorter = true,  -- override the generic sorter
-            override_file_sorter = true,     -- override the file sorter
-            case_mode = "smart_case",        -- or "ignore_case" or "respect_case"
-            -- the default case_mode is "smart_case"
-        }
-    }
-}
+require("telescope").setup({
+	pickers = {
+		find_files = {
+			hidden = true,
+			no_ignore = true,
+		},
+	},
+	defaults = {
+		prompt_prefix = "   ",
+		-- buffer_previewer_maker = new_maker,
+		file_ignore_patterns = {
+			"node_modules/",
+			".git/",
+			-- 'DS_'
+		},
+		vimgrep_arguments = {
+			"rg",
+			"--color=never",
+			"--no-heading",
+			"--with-filename",
+			"--line-number",
+			"--column",
+			"--smart-case",
+			"--ignore-file",
+			-- '.gitignore'
+		},
+	},
+	extensions = {
+		fzf = {
+			fuzzy = true, -- false will only do exact matching
+			override_generic_sorter = true, -- override the generic sorter
+			override_file_sorter = true, -- override the file sorter
+			case_mode = "smart_case", -- or "ignore_case" or "respect_case"
+			-- the default case_mode is "smart_case"
+		},
+	},
+})
 -- To get fzf loaded and working with telescope, you need to call
 -- load_extension, somewhere after setup function:
-require('telescope').load_extension('fzf')
-
+require("telescope").load_extension("fzf")
 
 -- =======================================================================
 -- ============================== REMAPS =================================
