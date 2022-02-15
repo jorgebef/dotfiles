65d83f03cc04bda6c90766615ae31dde8c3bc5e6
Jorge Befan <jorge.befan@gmail.com>
2022-02-15 21:30:09 +0100

    update to use findfile toggle for nvim-tree.lua

--- nvim/lua/nvim-tree-config.lua
+++ nvim/lua/nvim-tree-config.lua
@@ -62,4 +62,4 @@ require("nvim-tree").setup({
 local remap = vim.api.nvim_set_keymap
 local nsn_opts = { noremap = true, silent = true, nowait = true }
 
-remap("n", "<leader>e", ":NvimTreeToggle<CR>", nsn_opts)
+remap("n", "<leader>e", ":NvimTreeFindFileToggle<CR>", nsn_opts)
