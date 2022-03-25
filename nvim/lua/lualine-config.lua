a04422085a617632a1821d6048e6f2224faabfae
Jorge Befan <jorge.befan@gmail.com>
2022-03-25 22:40:42 +0100

    setting colors based on variables and cleaning up settings for neovim

--- nvim/lua/lualine-config.lua
+++ nvim/lua/lualine-config.lua
@@ -1,3 +1,30 @@
+local cp = {
+	rosewater = "#F5DFDA", -- Rosewater
+	flamingo = "#F2CBCB", -- Flamingo			-- done
+	mauve = "#C6A4F8", -- Mauve					-- done
+	pink = "#F5BFE7", -- Pink					-- done
+	red = "#F586A2", -- Red						-- done
+	maroon = "#EB9DA4", -- Maroon				-- done
+	peach = "#FCB081", -- Peach					-- done
+	yellow = "#F8DEA9", -- Yellow				-- done
+	green = "#A9E4A3", -- Green					-- done
+	blue = "#8CB3FA", -- Blue					-- done
+	sky = "#89DCFD", -- Sky						-- done
+	teal = "#88DFD2", -- Teal					-- done
+	lavender = "#C7CBFD", -- Lavender			-- done
+	white = "#D2DAF1", -- White					-- done
+	gray2 = "#B9C0DB", -- Gray2
+	gray1 = "#6F7599", -- Gray1
+	gray0 = "#5E6487", -- Gray0
+	black4 = "#3E435E", -- Black4
+	black3 = "#2B3045", -- Black3
+	black2 = "#24273A", -- Black2				-- done
+	black1 = "#1F2233", -- Black1
+	black0 = "#1A1B26", -- Black0
+}
+
+
+
 require("lualine").setup({
 	options = {
 		icons_enabled = true,
@@ -33,9 +60,9 @@ require("lualine").setup({
 				"diff",
 				colored = true,
 				diff_color = {
-					added = { fg="#ABE9B3", bg = nil }, -- Changes diagnostic's info color
-					modified = { fg="#FAE3B0", bg = nil }, -- changes diagnostic's warn color
-					removed = { fg="#F28FAD",bg=nil }, -- changes diagnostic's error color
+					added = { fg=cp.green, bg = nil }, -- Changes diagnostic's info color
+					modified = { fg=cp.yellow, bg = nil }, -- changes diagnostic's warn color
+					removed = { fg=cp.red,bg=nil }, -- changes diagnostic's error color
 				},
 				separator = { right = "" },
 				left_padding = 1,
@@ -51,10 +78,10 @@ require("lualine").setup({
 				sections = { "error", "warn", "info", "hint" },
 				-- all colors are in format #rrggbb
 				diagnostics_color = {
-					error = { fg="#F28FAD",bg=nil }, -- changes diagnostic's error color
-					warn = { fg="#FAE3B0", bg = nil }, -- changes diagnostic's warn color
-					info = { fg="#96CDFB", bg = nil }, -- Changes diagnostic's info color
-					hint = { fg="#F5E0DC", bg = nil }, -- Changes diagnostic's hint color
+					error = { fg=cp.red,bg=nil }, -- changes diagnostic's error color
+					warn = { fg=cp.yellow, bg = nil }, -- changes diagnostic's warn color
+					info = { fg=cp.blue, bg = nil }, -- Changes diagnostic's info color
+					hint = { fg=cp.rosewater, bg = nil }, -- Changes diagnostic's hint color
 				},
 				-- symbols = {error = 'E', warn = 'W', info = 'I', hint = 'H'}
 				symbols = { error = " ", warn = " ", info = " " },
