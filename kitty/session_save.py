77b7c85293a08216a66c19027859bba3269dd252
Jorge Befan <jorge.befan@gmail.com>
2022-05-31 22:52:12 +0200

    improve the session saving for kitty terminal

--- kitty/session_save.py
+++ kitty/session_save.py
@@ -10,26 +10,33 @@ dir = os.path.dirname(__file__)
 json_path = 'kitty-session.json'
 sess_dump_path = os.path.join(dir, json_path)
 
+def createSession(output, sessionData):
+    for os_window in sessionData:
+        for tab in os_window['tabs']:
+            output.write('new_tab '+tab['title'])
+            output.write('\n')
+            output.write('cd '+tab['windows'][0]['cwd'])
+            output.write('\n')
+            window_count = 0
+            for window in tab['windows']:
+                tabs = len(tab['windows'])
+                if window['is_focused']:
+                    output.write('focus')
+                    output.write('\n')
+                    window_count+=1
+                if tabs-window_count == 0:
+                    break
+                output.write('launch zsh')
+                output.write('\n')
+                window_count+=1
+
+
 with open(sess_dump_path) as f:
     sessData = json.load(f)
-    # set directory for the files
     sess_file_path = 'session.conf'
     sess_full_path = os.path.join(dir, sess_file_path)
     s = open(sess_full_path,'w')
-    for i in sessData:
-        for tab in i['tabs']:
-            s.write('new_tab '+tab['title'])
-            s.write('\n')
-            s.write('cd '+tab['windows'][0]['cwd'])
-            s.write('\n')
-            s.write('launch zsh')
-            s.write('\n')
-            # -------------- NOT YET WORKING --------------
-            # for window in tab['windows']:
-            #     s.write('cd '+tab['windows'][0]['cwd'])
-            #     s.write('\n')
-            #     s.write('launch zsh')
-            #     s.write('\n')
+    createSession(s,sessData)
     s.close()
-# close the file
+
 f.close()
