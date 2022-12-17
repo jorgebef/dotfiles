6521b12b18199c91867acbd97561241ad4bd403f
Jorge Befan <jorge.befan@gmail.com>
2022-12-18 00:32:25 +0100

    add packer commands and file to loaded files

--- nvim/lua/jbef/init.lua
+++ nvim/lua/jbef/init.lua
@@ -1,15 +1,16 @@
 -- ======================== REQUIRE EXTRA FILES ===================
 require("jbef.options")
 require("jbef.keymaps")
+require("jbef.packer")
 
 -- ==============================================
 -- ========= TURN ALL JSON INTO JSONC ===========
 -- ==============================================
 vim.api.nvim_create_augroup("jsoncComments", { clear = true })
 vim.api.nvim_create_autocmd({ "FileType" }, {
-  pattern = "json",
-  command = "set filetype=jsonc",
-  group = "jsoncComments",
+	pattern = "json",
+	command = "set filetype=jsonc",
+	group = "jsoncComments",
 })
 
 --[[ local envGroup = vim.api.nvim_create_augroup("__env", { clear = true }) ]]
@@ -30,15 +31,15 @@ vim.api.nvim_create_autocmd({ "FileType" }, {
 
 vim.api.nvim_create_augroup("highlight_yank", { clear = true })
 vim.api.nvim_create_autocmd({ "TextYankPost" }, {
-  pattern = "*",
-  command = 'lua vim.highlight.on_yank { higroup="IncSearch", timeout=250 }',
-  group = "highlight_yank",
+	pattern = "*",
+	command = 'lua vim.highlight.on_yank { higroup="IncSearch", timeout=250 }',
+	group = "highlight_yank",
 })
 
 vim.api.nvim_create_augroup("CursorLine", { clear = true })
 vim.api.nvim_create_autocmd(
-  { "VimEnter", "WinEnter", "BufRead", "BufWinEnter" },
-  { pattern = "*[^{T}]", command = 'setlocal cursorline | :echo""', group = "CursorLine" }
+	{ "VimEnter", "WinEnter", "BufRead", "BufWinEnter" },
+	{ pattern = "*[^{T}]", command = 'setlocal cursorline | :echo""', group = "CursorLine" }
 )
 vim.api.nvim_create_autocmd({ "WinLeave" }, { pattern = "*", command = "setlocal nocursorline", group = "CursorLine" })
 
