786ec45c4d0f7a819ff89752658ec4b09b8839d1
Jorge Befan <jorge.befan@gmail.com>
2023-04-03 12:42:37 +0200

    improve wezterm config

--- jbef/.config/wezterm/wezterm.lua
+++ jbef/.config/wezterm/wezterm.lua
@@ -1,15 +1,9 @@
 local wezterm = require("wezterm")
 
-return {
-  hide_tab_bar_if_only_one_tab = true,
-  window_padding = {
-    left = 5,
-    right = 5,
-    top = 10,
-    bottom = 10,
-  },
+local config = {
+  term = "wezterm",
   color_scheme = "Catppuccin Mocha",
-  window_background_opacity = 1.0,
+
   font = wezterm.font({ family = "JetBrainsMono Nerd Font" }),
   font_size = 15.0,
   font_rules = {
@@ -29,42 +23,35 @@ return {
       }),
     },
   },
-
   -- Disables ligatures
-  harfbuzz_features = { "calt=0", "clig=0", "liga=0" },
-
-  cursor_blink_rate = 0,
-  -- force_reverse_video_cursor = true,
-
-  -- https://wezfurlong.org/wezterm/config/keyboard-concepts.html?highlight=modifiers#keyboard-concepts:~:text=macOS%20Left%20and%20Right%20Option%20Key
-  send_composed_key_when_right_alt_is_pressed = true,
-
-  -- max_fps = 240,
-  -- max_fps = 180,
+  -- harfbuzz_features = { "calt=0", "clig=0", "liga=0" },
+  underline_thickness = 2,
   max_fps = 120,
-  -- max_fps = 60,
 
   automatically_reload_config = true,
-  check_for_updates = true,
-
-  -- enable_tab_bar = false,
   use_fancy_tab_bar = false,
   tab_bar_at_bottom = true,
-
-  -- window_close_confirmation = "NeverPrompt",
-
   window_decorations = "RESIZE",
-  -- initial_cols = 300,
-  -- initial_rows = 80,
-
-  -- unzoom_on_switch_pane = true,
+  -- window_background_opacity = 0.9,
+  -- macos_window_background_blur = 70,
+  hide_tab_bar_if_only_one_tab = true,
+  window_padding = {
+    left = 5,
+    right = 5,
+    top = 10,
+    bottom = 10,
+  },
 
   -- Enable CSI u mode
   -- https://wezfurlong.org/wezterm/config/lua/config/enable_csi_u_key_encoding.html
   -- This enables using C-. and C-, as keybindings in TMUX for example
   enable_csi_u_key_encoding = true,
-
   disable_default_key_bindings = false,
+  hide_mouse_cursor_when_typing = true,
+  cursor_blink_rate = 0,
+
+  -- https://wezfurlong.org/wezterm/config/keyboard-concepts.html?highlight=modifiers#keyboard-concepts:~:text=macOS%20Left%20and%20Right%20Option%20Key
+  send_composed_key_when_right_alt_is_pressed = true,
 
   keys = {
     { key = ".", mods = "CTRL", action = wezterm.action.SendString("\x1b[46;5u") },
@@ -72,7 +59,6 @@ return {
     -- This below maps CTRL+; to Ω, since that is a key combination that can be sent to tmux to run the popup with the session switcher
     { key = ";", mods = "CTRL", action = wezterm.action.SendString("Ω") },
   },
-
   mouse_bindings = {
     {
       -- Change the default click behavior so that it only selects
@@ -82,13 +68,13 @@ return {
       action = wezterm.action.CompleteSelection("ClipboardAndPrimarySelection"),
     },
 
-    -- and make CTRL-Click open hyperlinks
+    -- and make CMD-Click open hyperlinks
     {
       event = { Up = { streak = 1, button = "Left" } },
       mods = "CMD",
       action = wezterm.action.OpenLinkAtMouseCursor,
     },
   },
-
-  term = "wezterm",
 }
+
+return config
