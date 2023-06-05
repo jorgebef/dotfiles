e314228cd94bfc679897ee94d3c0f2b21a1b226b
Jorge Befan <jorge.befan@gmail.com>
2023-06-05 09:03:11 +0200

    detect if typescriptreact

--- jbef/.config/nvim/lua/config/autocommands.lua
+++ jbef/.config/nvim/lua/config/autocommands.lua
@@ -1,37 +1,35 @@
 -- TURN ALL JSON INTO JSONC
 vim.api.nvim_create_augroup("jsoncComments", { clear = true })
 vim.api.nvim_create_autocmd({ "FileType" }, {
-	pattern = "json",
-	command = "set filetype=jsonc",
-	group = "jsoncComments",
+  pattern = "json",
+  command = "set filetype=jsonc",
+  group = "jsoncComments",
 })
-
---[[ local envGroup = vim.api.nvim_create_augroup("__env", { clear = true }) ]]
---[[ vim.api.nvim_create_autocmd("BufEnter", { ]]
---[[ 	pattern = ".env*", ]]
---[[ 	group = envGroup, ]]
---[[ 	callback = function(args) ]]
---[[ 		vim.diagnostic.disable(args.buf) ]]
---[[ 	end, ]]
---[[ }) ]]
-
--- vim.api.nvim_create_augroup("jsoncComments", { clear = true })
--- vim.api.nvim_create_autocmd({ "BufEnter" }, {
--- 	pattern = "*config.json",
--- 	command = "set filetype=jsonc",
--- 	group = "jsoncComments",
--- })
 
 vim.api.nvim_create_augroup("highlight_yank", { clear = true })
 vim.api.nvim_create_autocmd({ "TextYankPost" }, {
-	pattern = "*",
-	command = 'lua vim.highlight.on_yank { higroup="IncSearch", timeout=250 }',
-	group = "highlight_yank",
+  pattern = "*",
+  command = 'lua vim.highlight.on_yank { higroup="IncSearch", timeout=250 }',
+  group = "highlight_yank",
 })
 
 vim.api.nvim_create_augroup("CursorLine", { clear = true })
 vim.api.nvim_create_autocmd(
-	{ "VimEnter", "WinEnter", "BufRead", "BufWinEnter" },
-	{ pattern = "*[^{T}]", command = 'setlocal cursorline | :echo""', group = "CursorLine" }
+  { "VimEnter", "WinEnter", "BufRead", "BufWinEnter" },
+  { pattern = "*[^{T}]", command = 'setlocal cursorline | :echo""', group = "CursorLine" }
 )
 vim.api.nvim_create_autocmd({ "WinLeave" }, { pattern = "*", command = "setlocal nocursorline", group = "CursorLine" })
+
+-- Set global variable named typescriptDefinition for conditonal lsp keymaps
+vim.api.nvim_create_augroup("TypescriptDetect", { clear = true })
+vim.api.nvim_create_autocmd({ "BufEnter", "BufWinEnter", "BufRead", "VimEnter", "WinEnter" }, {
+  pattern = { "*" },
+  callback = function()
+    if vim.bo.filetype == "typescriptreact" then
+      _G.isTypescriptReact = true
+    else
+      _G.isTypescriptReact = false
+    end
+  end,
+  group = "TypescriptDetect",
+})
