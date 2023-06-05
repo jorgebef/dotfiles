3116d43caa2207ec61d08a76c5f55d2d308032ac
Jorge Befan <jorge.befan@gmail.com>
2023-06-05 09:01:24 +0200

    Use different branch of colorizer to take advantage of virtual text in nvim 0.10

--- jbef/.config/nvim/lua/config/plugins/colorizer.lua
+++ jbef/.config/nvim/lua/config/plugins/colorizer.lua
@@ -1,4 +1,8 @@
-local M = { "NvChad/nvim-colorizer.lua" } -- Colorizer
+local M = {
+  -- "NvChad/nvim-colorizer.lua",
+  "mehalter/nvim-colorizer.lua",
+  branch = "master",
+} -- Colorizer
 
 function M.config()
   -- ===================================================================
@@ -6,29 +10,35 @@ function M.config()
   -- It has the virtualtext option, but needs optimizing
   -- ===================================================================
   require("colorizer").setup({
-    filetypes = { "*" },
+    filetypes = { "*", "!lazy", "!TelescopePrompt" },
     user_default_options = {
-      -- RGB = true, -- #RGB hex codes
-      -- RRGGBB = true, -- #RRGGBB hex codes
-      -- names = true, -- "Name" codes like Blue or blue
-      -- RRGGBBAA = false, -- #RRGGBBAA hex codes
-      -- AARRGGBB = false, -- 0xAARRGGBB hex codes
-      -- rgb_fn = true, -- CSS rgb() and rgba() functions
-      -- hsl_fn = false, -- CSS hsl() and hsla() functions
-      -- css = false, -- Enable all CSS features: rgb_fn, hsl_fn, names, RGB, RRGGBB
-      -- css_fn = false, -- Enable all CSS *functions*: rgb_fn, hsl_fn
+      RGB = true, -- #RGB hex codes
+      RRGGBB = true, -- #RRGGBB hex codes
+      names = true, -- "Name" codes like Blue or blue
+      RRGGBBAA = true, -- #RRGGBBAA hex codes
+      AARRGGBB = true, -- 0xAARRGGBB hex codes
+      rgb_fn = true, -- CSS rgb() and rgba() functions
+      hsl_fn = true, -- CSS hsl() and hsla() functions
+      css = true, -- Enable all CSS features: rgb_fn, hsl_fn, names, RGB, RRGGBB
+      css_fn = true, -- Enable all CSS *functions*: rgb_fn, hsl_fn
       -- Available modes for `mode`: foreground, background,  virtualtext
-      mode = "background", -- Set the display mode.
+      -- mode = "background", -- Set the display mode.
+      mode = "inline", -- Set the display mode.
       -- mode = 'virtualtext', -- Set the display mode.
       -- Available methods are false / true / "normal" / "lsp" / "both"
       -- True is same as normal
       tailwind = true, -- Enable tailwind colors
       -- parsers can contain values used in |user_default_options|
       -- sass = { enable = false, parsers = { css } }, -- Enable sass colors
-      -- virtualtext = "■",
+      virtualtext = " ",
     },
     -- all the sub-options of filetypes apply to buftypes
-    buftypes = {},
+    buftypes = {
+      "*",
+      -- exclude prompt and popup buftypes from highlight
+      "!prompt",
+      "!popup",
+    },
   })
 
   vim.keymap.set(
