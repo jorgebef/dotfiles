ea3449b56cdea7323b354592ed43bbf33b644737
jorgebef <jorge.befan@gmail.com>
2025-07-27 21:48:01 +0200

    switch over to nix-darwin for declarative configuration!

---
+++ jbef/.config/nix/flake.nix
@@ -0,0 +1,104 @@
+{
+  description = "jbef nix-darwin system flake";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nix-darwin.url = "github:nix-darwin/nix-darwin/master";
+    nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
+
+    nix-homebrew.url = "github:zhaofengli/nix-homebrew";
+
+    # Optional: Declarative tap management
+    homebrew-core = {
+      url = "github:homebrew/homebrew-core";
+      flake = false;
+    };
+    homebrew-cask = {
+      url = "github:homebrew/homebrew-cask";
+      flake = false;
+    };
+  };
+
+  outputs = inputs @ {
+    self,
+    nix-darwin,
+    nixpkgs,
+    nix-homebrew,
+    homebrew-core,
+    homebrew-cask,
+  }: let
+    configuration = {pkgs, ...}: {
+      # List packages installed in system profile. To search by name, run:
+      # $ nix-env -qaP | grep wget
+      environment.systemPackages = [
+        pkgs.aerospace
+        pkgs.fish
+        pkgs.fzf
+        pkgs.neovim
+        pkgs.raycast
+        pkgs.starship
+        pkgs.sesh
+        pkgs.tmux
+        pkgs.vim
+      ];
+
+      fonts.packages = with pkgs; [
+        jetbrains-mono
+      ];
+
+      homebrew = {
+        enable = true;
+        casks = [
+          "iina"
+          "ghostty"
+        ];
+        # onActivation.cleanup = "zap";
+      };
+
+      system.primaryUser = "jbef";
+
+      # Necessary for using flakes on this system.
+      nix.settings.experimental-features = "nix-command flakes";
+
+      # Enable alternative shell support in nix-darwin.
+      programs.fish.enable = true;
+
+      # Set Git commit hash for darwin-version.
+      system.configurationRevision = self.rev or self.dirtyRev or null;
+
+      # Used for backwards compatibility, please read the changelog before changing.
+      # $ darwin-rebuild changelog
+      system.stateVersion = 6;
+
+      # The platform the configuration will be used on.
+      nixpkgs.hostPlatform = "aarch64-darwin";
+
+      # This allows to install non-free software
+      nixpkgs.config.allowUnfree = true;
+    };
+  in {
+    # Build darwin flake using:
+    # $ darwin-rebuild build --flake .#jbef
+    darwinConfigurations."jbef" = nix-darwin.lib.darwinSystem {
+      modules = [
+        configuration
+        nix-homebrew.darwinModules.nix-homebrew
+        {
+          nix-homebrew = {
+            # Install Homebrew under the default prefix
+            enable = true;
+            # Apple Silicon Only: Also install Homebrew under the default Intel prefix for Rosetta 2
+            enableRosetta = true;
+            # User owning the Homebrew prefix
+            user = "jbef";
+            # # Automatically migrate existing Homebrew installations
+            # autoMigrate = true;
+          };
+        }
+      ];
+    };
+
+    # Expose the package set, including overlays, for convenience.
+    darwinPackages = self.darwinConfigurations."jbef".pkgs;
+  };
+}
